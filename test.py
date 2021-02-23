02b492734e57d6a26be56b6565ae93afc549fda9
pparrott <47132660+pparrott@users.noreply.github.com>
2021-02-23 21:32:22 +0100

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-#! python3
-
-string = '/usr/local/opt/python@3.8/bin:/Library/Frameworks/Python.framework/Versions/3.8/bin:/Users/Phil/anaconda3/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Applications/VMware Fusion.app/Contents/Public:/opt/X11/bin'
-
-for i in string.split(':'):
-    print(i)
